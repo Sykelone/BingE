83f17356a7c76f07252e76ddfce154ef1756cfbc
Anubhav Kumar Singh <aksanubhav5@gmail.com>
2023-03-08 01:52:19 +0530

    Navbar added.

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from './Footer'
+
+export default function Home() {
+  return (
+    <div>
+        <div><Navbar/></div>
+        <div>Body</div>
+        <div><Footer/></div>
+        
+    </div>
+  )
+}
