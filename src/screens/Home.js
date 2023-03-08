89727c7937cf6e8d356a872b1a187a2920526868
Anubhav Kumar Singh <aksanubhav5@gmail.com>
2023-03-08 16:53:40 +0530

    front end

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 import Navbar from '../components/Navbar'
 import Footer from './Footer'
 
 export default function Home() {
   return (
     <div>
-        <div><Navbar/></div>
-        <div>This is body!</div>
-        <div><Footer/></div>
-        
+      <div><Navbar /></div>
+      <div>
+        <Carousel />
+      </div>
+      <div className='m-3'>
+        <Card />
+
+      </div>
+
+      <div><Footer /></div>
+
     </div>
   )
 }
